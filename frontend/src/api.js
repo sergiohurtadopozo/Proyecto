95b8cc020c8f4f7ee42a02fe616939612cd3990e
sergiohurtadopozo <148066531+sergiohurtadopozo@users.noreply.github.com>
2025-05-04 19:25:31 +0200

    act

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 // src/api.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_API_URL;
 
 // Configurar axios para incluir el token en todas las peticiones
 axios.interceptors.request.use(
